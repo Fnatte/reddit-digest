8a00f286d17a45aefbabc75366886457c7aa67d0
Anton Niklasson <niklasson.anton@gmail.com>
2019-01-17 19:01:39 +0800

    move things around

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+ssh root@95.85.43.196 <<'ENDSSH'
+cd /var/www/reddit-digest
+git reset --hard HEAD && git clean -f -d
+git pull origin master
+yarn
+ENDSSH
+
--- mail-mockup/reddit.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios"
-
-export const getTopPostsFromSubreddits = () => {
-    return axios("http://localhost:8888/digest").then(res => res.data)
-}
