0dd32f9849d055b019720bab3a070716a2d2c029
Anton Niklasson <niklasson.anton@gmail.com>
2019-01-19 15:10:37 +0800

    hmm

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM node:8
 COPY . .
+RUN yarn
 EXPOSE 8888
 CMD ["node", "src/app.js"]
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 ssh root@95.85.43.196 <<'ENDSSH'
 cd /var/www/reddit-digest
-git reset --hard HEAD && git clean -f -d
+git reset --hard HEAD
 git pull origin master
-yarn
 docker build -t reddit-digest .
 docker stop reddit-digest-container
 docker run --rm -d -p 5000:5000 --name reddit-digest-container reddit-digest
